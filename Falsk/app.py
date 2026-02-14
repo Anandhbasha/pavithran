55f393f7bcdc411091c2768b4619f770d6d763c7
Anandhbasha <anadhakm@gmail.com>
2026-02-14 20:28:06 +0530

    flask

--- Falsk/app.py
+++ Falsk/app.py
@@ -1,44 +1,65 @@
 # pip install flask
 
 from flask import Flask,jsonify,request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-users = []
+# mongodb connection
+# pip install pymongo
+client = MongoClient("mongodb://127.0.0.1:27017/")
+db = client["NewStudentBataBase"]
+collection = db["NewDb"]
+print("Db is Connect")
 
 @app.route('/',methods=["GET"])
-def home():
-    if len(users)==0:
-        return "No records"
-    return jsonify(users),200
+def getUsers():
+    users = []
+    for user in collection.find():
+        user["_id"] = str(user["_id"])
+        users.append(user)
+    return jsonify(users)
 
 @app.route('/add',methods=["POST"])
 def addUser():
     data = request.json
-    users.append(data)
+    res = collection.insert_one(data)
     return jsonify({
-        "message":"User Added Successfully","users":users
+        "message":"User Added Successfully","_id":str(res.inserted_id)
     }),201
 
-@app.route('/editUser/<string:userName>',methods=['PUT'])
-def updateUser(userName):
+@app.route('/editUser/<id>',methods=['PUT'])
+def updateUser(id):
     data = request.json
-    for user in users:
-        if user["userName"] ==userName:
-            user['userAge'] = data.get("userAge",user['userAge'])
-            user['userAddress'] = data.get("userAddress",user['userAddress'])
-        return jsonify({
-        "message":"User Updated Successfully","users":users
+    res = collection.update_one({
+        "_id":ObjectId(id)
+    },{"$set":data})
+    if res.matched_count==0:
+        return jsonify({"error":"User Not found"}),404
+    return jsonify({
+        "message":"User Updated Successfully"
     }),201
 
 
-@app.route('/deleteUser/<string:userName>',methods=['DELETE'])
-def delUser(userName):
-    for user in users:
-        if user["userName"] ==userName:
-            users.remove(user)
+@app.route('/deleteUser/<id>',methods=['DELETE'])
+def delUser(id):
+    res = collection.delete_one({"_id":ObjectId(id)})
+    if res.deleted_count==0:
         return jsonify({
-        "message":"User Deleted Sucessfully Successfully"
-    }),404
+            "error":"User Not found"
+        }),404
+    return jsonify({
+        "message":"User Deleted Success fully"
+    })
+
+# read one
+@app.route("/user/<id>",methods=["GET"])
+def getUser(id):
+    user = collection.find_one({"_id":ObjectId(id)})
+    if not user:
+        return jsonify({"error":"User Not found"}),404
+    user["_id"]= str(user["_id"])
+    return jsonify(user)
 if __name__ =="__main__":
     app.run(debug=True)
