451fc18299763d2a89d733311cc8712802170ad4
Anandhbasha <anadhakm@gmail.com>
2026-02-28 20:48:25 +0530

    threads

---
+++ Threading/threads.py
@@ -0,0 +1,17 @@
+import threading
+
+import time
+
+def task(name):
+    for i in range(3):
+        print(f'{name}running{i}')
+        time.sleep(1)
+
+t1 = threading.Thread(target=task,args=("Thread1",))
+t2 = threading.Thread(target=task,args=("Thread2",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
